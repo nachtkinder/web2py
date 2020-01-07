7ffa85e0afc788905ab3bb91af514cac1ab3d4ec
nachtkinder <github.nachtkinder@spamgourmet.com>
2020-01-07 12:02:53 +0000

    Added foreign key detection and stubs for future functionality.

--- scripts/extract_mysql_models.py
+++ scripts/extract_mysql_models.py
@@ -20,11 +20,17 @@ Please complete it, improve it and continue.
 
 Created by Falko Krause, minor modifications by Massimo Di Pierro, Ron McOuat and Marvi Benedet
 '''
+from __future__ import print_function
 import subprocess
 import re
 import sys
-data_type_map = dict(
-    enum='enum',
+from collections import OrderedDict
+
+COLUMN_REGEX = re.compile(r'(?P<name>\S+)\s+(?P<type>\S+?)'
+                          r'(\((?P<options>[^)]+)\))?(,| )( .*)?')
+
+DATA_TYPE_MAP = dict(
+    enum='string',
     varchar='string',
     int='integer',
     integer='integer',
@@ -52,6 +58,10 @@ data_type_map = dict(
     longtext='text',
 )
 
+FKEY_REGEX = re.compile(r'FOREIGN KEY \(`(?P<name>[^`]+)`\) REFERENCES '
+                        r'(`(?P<schema>[^`]+)`\.)?(`(?P<table>[^`]+)`) '
+                        r'\(`(?P<column>[^`]+)`\)')
+
 
 def mysql(database_name, username, password, host, port):
     p = subprocess.Popen(['/usr/bin/mysql',
@@ -71,7 +81,7 @@ def mysql(database_name, username, password, host, port):
         username, password, host, port, database_name)
     legacy_db_table_web2py_code = []
     for table_name in tables:
-        #get the sql create statement
+        # get the sql create statement
         p = subprocess.Popen(['mysqldump',
                               '-u' + username,
                               '-p' + password,
@@ -80,10 +90,11 @@ def mysql(database_name, username, password, host, port):
                               '--skip-triggers',
                               '--skip-add-drop-table',
                               '--no-data', database_name,
-                              table_name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                              table_name], stdin=subprocess.PIPE,
+                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         sql_create_stmnt, stderr = p.communicate()
         if 'CREATE' in sql_create_stmnt:  # check if the table exists
-            #remove garbage lines from sql statement
+            # remove garbage lines from sql statement
             sql_lines = sql_create_stmnt.split('\n')
             sql_lines = filter(
                 lambda x: not(x in ('','\r') or x[:2] in ('--', '/*')),
@@ -96,31 +107,57 @@ def mysql(database_name, username, password, host, port):
             except AttributeError:
                 continue
 
-            fields = []
+            fields = OrderedDict()
             for line in sql_lines[1:-1]:
-                if ('KEY' in line) or ('PRIMARY' in line) or (' ID' in line) or line.startswith(')'):
+                line = line.strip()
+                if (' ID' in line) or line.startswith(')'):
                     continue
-                hit = re.search(r'(?P<name>\S+)\s+(?P<type>\S+?)(\((?P<options>[^)]+)\))?(,| )( .*)?', line)
-                if hit:
-                    name, d_type, options = hit.group('name'), hit.group('type'), hit.group('options')
-                    name = re.sub('`', '', name)
-                    if d_type == 'enum':
-                        requires =  ', requires=IS_IN_SET((%s))' % options
-                    elif 'NOT NULL' in line:
-                        requires = ', requires=IS_NOT_EMPTY()'
-                    else:
-                        requires = ''
-                    field_type = data_type_map[d_type]
-                    if field_type == 'string':
-                        try:
-                            length = ', length=%d' % int(options)
-                        except ValueError:
-                            length = ''
-                    else:
-                        length = ''
+                elif 'PRIMARY' in line:
+                    continue
+                elif 'UNIQUE' in line:
+                    continue
+                elif 'FOREIGN KEY' in line:
+                    fk_match = re.search(FKEY_REGEX, line)
+                    affected_field = fields[fk_match.group('name')]
+                    affected_field['type'] = 'references %s' % fk_match.group('table')
 
-                    web2py_table_code += "\n    Field('%s', '%s'%s%s)," % (
-                        name, field_type, requires, length)
+                    try:
+                        del affected_field['requires'], affected_field['length']
+                    except KeyError:
+                        pass
+                elif line.startswith('KEY'):
+                    continue
+                else:
+
+                    hit = re.search(COLUMN_REGEX, line)
+
+                    if hit:
+                        name = re.sub('`', '', hit.group('name'))
+                        options = hit.group('options')
+                        current_field = OrderedDict()
+                        is_enum = False
+                        current_field['type'] = hit.group('type')
+
+                        if current_field['type'] == 'enum':
+                            is_enum = True
+                            current_field['requires'] =  ', requires=IS_IN_SET((%s))' % options
+                        elif 'NOT NULL' in line:
+                            current_field['requires'] = ', requires=IS_NOT_EMPTY()'
+
+                        current_field['type'] = DATA_TYPE_MAP[current_field['type']]
+
+                        if (current_field['type'] == 'string') and not is_enum:
+                            try:
+                                current_field['length'] = ', length=%d' % int(options)
+                            except ValueError:
+                                pass
+
+                        fields[name] = current_field
+
+            for field_name, field_def in fields.items():
+                    web2py_table_code += "\n    Field('%s', '%s'%s)," % (
+                        field_name, field_def.popitem(last=False)[1], ''.join(field_def.values()))
+
             web2py_table_code = "legacy_db.define_table('%s',%s\n    migrate=False)" % (table_name, web2py_table_code)
             legacy_db_table_web2py_code.append(web2py_table_code)
     #----------------------------------------
@@ -130,9 +167,12 @@ def mysql(database_name, username, password, host, port):
         legacy_db_table_web2py_code)
     return legacy_db_web2py_code
 
-regex = re.compile(r'(?P<username>.*):(?P<password>.*)@(?P<host>.*)?(?P<port>:\d+)/(?P<db_name>.*)')
+regex = re.compile(r'(?P<username>.*):(?P<password>.*)@(?P<host>.*)?'
+                   r'(?P<port>:\d+)/(?P<db_name>.*)')
+
 if len(sys.argv) < 2 or not regex.match(sys.argv[1]):
-    print 'USAGE:\n\n    extract_mysql_models.py username:password@[host][:port]/data_basename\n\n'
+    print('USAGE:\n\n    extract_mysql_models.py username:password@[host]'
+          '[:port]/data_basename\n\n')
 else:
     m = regex.match(sys.argv[1])
     username = m.group('username')
@@ -140,4 +180,5 @@ else:
     host     = m.group('host') or 'localhost'
     port     = m.group('port')[1:] or '3309'
     db_name  = m.group('db_name')
-    print mysql(database_name=db_name, username=username, password=password, host=host, port=port)
+    print(mysql(database_name=db_name, username=username, password=password,
+                host=host, port=port))
